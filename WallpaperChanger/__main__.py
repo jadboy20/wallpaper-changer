1bd5cdcd370ed00316642fe13e61a00c5616a4b9
James Dixon <jamesdixon117@gmail.com>
2019-10-26 10:32:33 +1100

    Adding command line arguments

--- WallpaperChanger/__main__.py
+++ WallpaperChanger/__main__.py
@@ -3,9 +3,18 @@ import sys
 import WallpaperChanger.app as app
 
 
-def main():
-    app.App()
+def main(args):
+    app.App(args)
 
+
+def initialise_args(self):
+    """Create and return an argparse object."""
+    args = argparse.ArgumentParser()
+
+    # Add arguments
+    args.add_argument("--set-speed", help="Set the rate at which wallpapers cycle.")
+
+    return args
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
--- WallpaperChanger/app.py
+++ WallpaperChanger/app.py
@@ -1,11 +1,21 @@
 import os
 import sys
 import WallpaperChanger.wallpaper as wallpaper
+import argparse
+
 
 class App(object):
 
-    def __init__(self):
+    def __init__(self, args):
         self.run()
+        self.app_name = args[0]
+        self.args = self.initialise_args()
+
+
+
 
     def run(self):
-        wp = wallpaper.Wallpaper()
+        # wp = wallpaper.Wallpaper()
+        pass
+
+
