de57a20ecea41327ff300d0edcfa45744529f4d2
James Dixon <jdixon@halytech.com.au>
2020-01-31 16:28:51 +1100

    Added search option

--- WallpaperChanger/scraper/scraper.py
+++ WallpaperChanger/scraper/scraper.py
@@ -2,10 +2,17 @@ import sys
 import requests
 
 
+# There should be an option to cache the image links of download them. That way they
+# don't take up too much space. However, it will increase the amount of data usage.
+
 class Scraper(object):
     def __init__(self):
         pass
 
     def run(self):
         pass
+
+class PictureLink(object):
+    def __init__(self, title=None, link=None):
+        self.link =
 
