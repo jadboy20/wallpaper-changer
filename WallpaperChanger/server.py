fd1f34d0868a7f38f9373cba15f1f87324b20fa0
James Dixon <jamesdixon117@gmail.com>
2020-02-04 21:27:14 +1100

    Added a server module. Will need to fill it with stuff

---
+++ WallpaperChanger/server.py
@@ -0,0 +1,23 @@
+import sys
+import multiprocessing
+import socket
+import os
+import select
+from . import config
+
+
+# We put the server module in the same directory as the wallpaper changer as it requires modules such as config.
+
+class Server(object):
+    pass
+
+
+def main():
+    # Run a server
+
+    while True:
+
+
+
+if __name__ == "__main__":
+    main()
